4c0fca373a25c635db54869534a575bf32d2ac9f
zhangjinyangnwpu <ttblue888@gmail.com>
2018-03-04 23:42:14 +0800

    two sum

---
+++ 001_two_sum/001_two_sum.py
@@ -0,0 +1,14 @@
+class Solution:
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        for i in range(len(nums)):
+            for j in range(i+1,len(nums)):
+                if nums[i] + nums[j] == target:
+                    return [i,j]
+
+S = Solution()
+print(S.twoSum(nums=[2,7,11,15],target=9))
