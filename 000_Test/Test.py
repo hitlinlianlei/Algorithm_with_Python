1395bdc188c6b4bbf2c21d51850edba685e2e04a
zhangjinyangnwpu <ttblue888@gmail.com>
2018-03-29 23:23:27 +0800

    nothing

---
+++ 000_Test/Test.py
@@ -0,0 +1,6 @@
+import os
+import re
+
+re.compile(r'^hello[abcd]+world$')
+
+path = open('./')
--- 001_two_sum/001_two_sum.py
+++ 001_two_sum/001_two_sum.py
@@ -6,9 +6,10 @@ class Solution:
         :rtype: List[int]
         """
         for i in range(len(nums)):
-            for j in range(i+1,len(nums)):
+            for j in range(i + 1, len(nums)):
                 if nums[i] + nums[j] == target:
-                    return [i,j]
+                    return [i, j]
+
 
 S = Solution()
-print(S.twoSum(nums=[2,7,11,15],target=9))
+print(S.twoSum(nums=[2, 7, 11, 15], target=9))
