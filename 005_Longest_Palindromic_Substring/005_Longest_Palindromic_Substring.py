3eb2beb5d32987eaa148d44ea4fa275ff9421f92
zhangjinyangnwpu <ttblue888@gmail.com>
2018-11-29 17:01:42 +0800

    4

---
+++ 005_Longest_Palindromic_Substring/005_Longest_Palindromic_Substring.py
@@ -0,0 +1,12 @@
+import os
+class Solution:
+    def longestPalindrome(self, s):
+        """
+        :type s: str
+        :rtype: str
+        """
+
+        return s
+
+s = 'abcbac'
+print(Solution().longestPalindrome(s))
